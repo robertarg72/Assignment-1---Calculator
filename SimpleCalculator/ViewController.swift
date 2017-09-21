929e85e7ffceff2ba73bd710a55dc9839eea8441
Robert Argume <robert@Roberts-MacBook-Pro.local>
2017-09-20 22:58:40 -0400

    initial commit

---
+++ SimpleCalculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SimpleCalculator
+//
+//  Created by Robert Argume on 2017-09-20.
+//  Copyright © 2017 Robert Argume. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
